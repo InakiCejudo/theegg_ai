70115c18c8f6b4feb5646906fea9c92de6f0d75c
InakiCejudo <67116743+InakiCejudo@users.noreply.github.com>
2020-06-23 19:05:12 +0200

    Create tarea_21.py

---
+++ tarea_21/tarea_21.py
@@ -0,0 +1,21 @@
+x = 0.375
+num = int(x*10000)
+den = int(10000)
+s = 0
+while s != 1:
+	if (num % 7 == 0 and den % 7 == 0):
+		num = num / 7
+		den = den / 7
+	elif (num % 5 == 0 and den % 5 == 0):
+		num = num / 5
+		den = den / 5
+	elif (num % 3 == 0 and den % 3 == 0):
+		num = num / 3
+		den = den / 3
+	elif (num % 2 == 0 and den % 2 == 0):
+		num = num / 2
+		den = den / 2
+	else:
+		s = 1
+		print(num,den,sep='/')
+		print('fracción irreducible')
